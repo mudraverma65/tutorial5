5dca19df2d7fd4e1e0a318f92393321521a15025
Mudra Verma <md393822@dal.ca>
2023-07-05 15:05:19 -0300

    \users

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask,request
-import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 class User:
-    def __init__(self,userName, email):
-        self.userName = userName
+    def __init__(self, email, firstName, id):
         self.email = email
+        self.firstName = firstName
+        self.id = id
 
 listOfUsers = []
 
-user1 = User("user1","user1@gmail.com")
-user2 = User("user2","user2@gmail.com")
+user1 = User("abc@abc.ca", "ABC", "5abf6783")
+user2 = User("xyz@xyz.ca", "XYZ", "5abf674563")
 
 listOfUsers.append(user1)
 listOfUsers.append(user2)
 
-@app.route('/getMethod', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def getMethod():
-    jsonList = json.dumps([ob.__dict__ for ob in listOfUsers])
-    return jsonList
+    response = {
+        "message": "Users retrieved",
+        "success": True,
+        "users": [user.__dict__ for user in listOfUsers]
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
