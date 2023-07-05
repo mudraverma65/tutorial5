ccd967a6a677c9fed498d47c0f76a115045b4832
Mudra Verma <md393822@dal.ca>
2023-07-05 14:21:08 -0300

    Get List of Users

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask,request
+import json
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self,userName, email):
+        self.userName = userName
+        self.email = email
+
+listOfUsers = []
+
+user1 = User("user1","user1@gmail.com")
+user2 = User("user2","user2@gmail.com")
+
+listOfUsers.append(user1)
+listOfUsers.append(user2)
+
 @app.route('/getMethod', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
+def getMethod():
+    jsonList = json.dumps([ob.__dict__ for ob in listOfUsers])
+    return jsonList
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
